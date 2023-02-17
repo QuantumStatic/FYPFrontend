c07f2e23e137f73702a8d1cc5ff3b035754ab8b4
Utkarsh Jain <67118602+QuantumStatic@users.noreply.github.com>
2023-02-17 12:52:44 +0800

    Improved responsiveness for phone

--- front-end/src/components/InputArea.js
+++ front-end/src/components/InputArea.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 import styles from './InputArea.module.css'; 
+import { isMobile } from 'react-device-detect';
 
 
 export class InputArea extends Component {
+
+    getFormStyle() {
+        let style = {
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            margin: "auto",
+            height: "100%",
+            padding: "1%",
+            resize: "inherit",
+        };
+        style["width"] = isMobile ? "90%" : "60%";
+        return style;
+    }
+
     render() {
         return (
             <React.Fragment>
-                
-                <form action="https://httpbin.org/get" method="get">
+                <form style = {this.getFormStyle()} action="https://httpbin.org/get" method="get">
                     <fieldset>
                         <legend align="left">Input Area</legend>
-                                {/* Text Box Code */}
-                                <label htmlFor = "inputText" /> 
-                                <textarea name= "inputText" id="inputText" placeholder="Paste your text here..." autoComplete="on" required autoFocus/>
+                            {/* Text Box Code */}
+                            <label htmlFor = "inputText" /> 
+                            <textarea name= "inputText" id="inputText" placeholder="Paste your text here..." autoComplete="on" required autoFocus/>
 
-                                {/* Upload File Code */}
-                                
-                                <br />
+                            {/* Upload File Code */}
+                            
+                            <br />
 
-                                {/* Submit/Reset Button Code */}
-                                <button type="submit">Submit</button>
-                                <button type='reset'>Reset</button>
+                            {/* Submit/Reset Button Code */}
+                            <button type="submit">Submit</button>
+                            <button type='reset'>Reset</button>
                     </fieldset>
                 </form>
-                
             </React.Fragment>
         );
     }
--- front-end/src/components/OutputBox.js
+++ front-end/src/components/OutputBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './OutputBox.css';
+import { isMobile } from 'react-device-detect';
 
 export class OutputBox extends Component {
 
@@ -7,11 +8,19 @@ export class OutputBox extends Component {
         return "If you are using the display property to define your div as a flex container, then you can't use the method above to center text vertically within the div. Instead, you have to use the align-items property with the value center. Let's create the same div as above, giving it the class name .center. Set display to flex to make the div a flex container. To more clearly see that the text is vertically centered, define the height property as well. Otherwise, the flex container will only be as tall as it needs to be to contain the paragraph inside. Then, set the align-items property to center."
     }
 
+    getOutputAreaContainerID() {
+        if (isMobile) {
+            return "ouputAreaContainerMobile";
+        } else {
+            return "ouputAreaContainer";
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
                 
-                <div id="ouputAreaContainer">
+                <div id={this.getOutputAreaContainerID()}>
                     <fieldset>
                         <legend align="left">Output Area</legend>
                         <div id="outputTextBox">
